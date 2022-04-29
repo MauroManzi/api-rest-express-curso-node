67040eb7a6e68b7c0fe650d91586210b5f8f0839
MauroManzi <manzimauro909@gmail.com>
2022-04-29 20:08:56 -0300

    Rutas

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const debug = require('debug')('app:inicio');
 const express = require('express');
 //const logger = require('./logger');
 const app = express();
-const Joi = require('joi');
 const morgan = require('morgan');
 const config = require('config');
+const usuarios = require('./routes/usuarios');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.use('/api/usuarios', usuarios);
 
 //Configuración de entornos
 console.log('Aplicación: ' + config.get('nombre'));
@@ -32,76 +33,10 @@ debug('Conectando con la base de datos');
 //     next();
 // })
 
-const usuarios = [
-    {id:1, nombre:'Mauro'},
-    {id:2, nombre:'Roberto'},
-    {id:3, nombre:'Ana'}
-];
-
 app.get('/', (req, res) => {
     res.send('Hola mundo desde express');
 
 }); 
-
-app.get('/api/usuarios', (req, res) => {
-    res.send (usuarios);
-});
-
-app.get('/api/usuarios/:id', (req, res) => {
-    let usuario = existeUsuario(req.params.id);
-    if(!usuario) res.status(404).send('El usuario no fue encontrado.');
-    res.send(usuario);
-});
-
-app.post('/api/usuarios', (req,res) =>{
-    const {error, value} = validarUsuario(req.body.nombre);
-    if(!error){
-        const usuario = {
-                id: usuarios.length + 1,
-                nombre: value.nombre
-            };
-            usuarios.push(usuario);
-            res.send(usuario); 
-    }
-    else{
-        //  404 Bad request
-        const mensaje = error.details[0].message;
-        res.status(400).send(mensaje);
-    }
-});
-
-app.put('/api/usuarios/:id',(req,res) =>{
-    let usuario = existeUsuario(req.params.id);
-    if(!usuario) {
-        res.status(404).send('El usuario no fue encontrado');
-        return;
-    }
-   
-    const {error, value} = validarUsuario(req.body.nombre);
-
-    if(error){
-        const mensaje = error.details[0].message;
-        res.status(400).send(mensaje);
-        return;
-    }
-    
-    usuario.nombre = value.nombre;
-
-
-})
-
-
-app.delete('/api/usuarios/:id',(req,res) =>{
-    let usuario = existeUsuario(req.params.id);
-    if(!usuario) {
-        res.status(404).send('El usuario no fue encontrado');
-        return
-    }
-    const index = usuarios.indexOf(usuario);
-    usuarios.splice(index, 1);
-    res.send(usuarios);
-})
-
 
 const port = process.env.PORT || 3000;
 
@@ -109,24 +44,6 @@ app.listen(port, () =>{
     // console.log('Escuchando en el puerto ' + port);
     console.log(`Escuchando en el puerto ${port}`);
 })
-
-
-function existeUsuario(id){
-    let usuario = usuarios.find(u => u.id === parseInt(id));
-    return usuario;
-}
-
-function validarUsuario(nom){
-    const schema = Joi.object({
-        nombre: Joi.string()
-            .min(3)
-            .required(),
-    });  
-    return schema.validate({ nombre: nom});
-}
-
-
-
 
 
 //app.get(); //petición
---
+++ routes/usuarios.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const ruta = express.Router();
+const Joi = require('joi');
+
+const usuarios = [
+    {id:1, nombre:'Mauro'},
+    {id:2, nombre:'Roberto'},
+    {id:3, nombre:'Ana'}
+];
+
+ruta.get('/', (req, res) => {
+    res.send (usuarios);
+});
+
+ruta.get('/:id', (req, res) => {
+    let usuario = existeUsuario(req.params.id);
+    if(!usuario) res.status(404).send('El usuario no fue encontrado.');
+    res.send(usuario);
+});
+
+ruta.post('/', (req,res) =>{
+    const {error, value} = validarUsuario(req.body.nombre);
+    if(!error){
+        const usuario = {
+                id: usuarios.length + 1,
+                nombre: value.nombre
+            };
+            usuarios.push(usuario);
+            res.send(usuario); 
+    }
+    else{
+        //  404 Bad request
+        const mensaje = error.details[0].message;
+        res.status(400).send(mensaje);
+    }
+});
+
+ruta.put('/:id',(req,res) =>{
+    let usuario = existeUsuario(req.params.id);
+    if(!usuario) {
+        res.status(404).send('El usuario no fue encontrado');
+        return;
+    }
+   
+    const {error, value} = validarUsuario(req.body.nombre);
+
+    if(error){
+        const mensaje = error.details[0].message;
+        res.status(400).send(mensaje);
+        return;
+    }
+    
+    usuario.nombre = value.nombre;
+
+
+})
+
+
+ruta.delete('//:id',(req,res) =>{
+    let usuario = existeUsuario(req.params.id);
+    if(!usuario) {
+        res.status(404).send('El usuario no fue encontrado');
+        return
+    }
+    const index = usuarios.indexOf(usuario);
+    usuarios.splice(index, 1);
+    res.send(usuarios);
+})
+
+
+function existeUsuario(id){
+    let usuario = usuarios.find(u => u.id === parseInt(id));
+    return usuario;
+}
+
+function validarUsuario(nom){
+    const schema = Joi.object({
+        nombre: Joi.string()
+            .min(3)
+            .required(),
+    });  
+    return schema.validate({ nombre: nom});
+}
+
+
+module.exports = ruta;
